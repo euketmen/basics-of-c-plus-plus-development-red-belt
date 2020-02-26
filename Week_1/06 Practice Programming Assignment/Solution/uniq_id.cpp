5c8ef6b787030bf5938dc6d76c2f938b06a92de2
Hitoku <hitoku@list.ru>
2020-02-26 20:15:59 +0300

    add macro

---
+++ Week_1/06 Practice Programming Assignment/Solution/uniq_id.cpp
@@ -0,0 +1,20 @@
+//This is a personal academic project. Dear PVS-Studio, please check it.
+//PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+#include "test_runner.h"
+
+#include <iostream>
+
+//-------------------------------------------------------------------------------------------------
+#define UNIQ_ID_IMPL_2(lineno) _a_local_var_##lineno
+#define UNIQ_ID_IMPL(lineno) UNIQ_ID_IMPL_2(lineno)
+#define UNIQ_ID UNIQ_ID_IMPL(__LINE__)
+//-------------------------------------------------------------------------------------------------
+int main()
+{
+    int UNIQ_ID = 0;
+    std::string UNIQ_ID = "hello";
+    std::vector<std::string> UNIQ_ID = { "hello", "world" };
+    std::vector<int> UNIQ_ID = { 1, 2, 3, 4 };
+    return 0;
+}
+//-------------------------------------------------------------------------------------------------
